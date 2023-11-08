6016740231087699c8f71b36e3a00a52b3912d0d
VikasAgrawal-04 <vikash.agarawal2012@gmail.com>
2023-11-09 01:04:03 +0530

    model changed

--- packages/api/lib/src/caretaker/src/models/appointment/care_appointment_data.dart
+++ packages/api/lib/src/caretaker/src/models/appointment/care_appointment_data.dart
@@ -19,6 +19,27 @@ class CareData {
   final String? startdatetime;
   final String? finished;
 
+  @JsonKey(name: 'appointment_test')
+  List<AppointmentSubDetails>? appointmentTest;
+
+  @JsonKey(name: 'appointment_diet')
+  List<AppointmentSubDetails>? appointmentDiet;
+
+  @JsonKey(name: 'appointment_doctors_note')
+  List<AppointmentSubDetails>? appointmentDoctorsNote;
+
+  @JsonKey(name: 'appointment_prescription')
+  List<AppointmentSubDetails>? appointmentPrescription;
+
+  @JsonKey(name: 'appointment_test_notes')
+  List<AppointmentNotes>? appointmentTestNotes;
+
+  @JsonKey(name: 'appointment_diet_notes')
+  List<AppointmentNotes>? appointmentDietNotes;
+
+  @JsonKey(name: 'appointment_doctors_note_notes')
+  List<AppointmentNotes>? appointmentDoctorsNoteNotes;
+
   CareData(
       {this.apptid,
       this.visitdate,
@@ -33,10 +54,49 @@ class CareData {
       this.taxineeded,
       this.startappointment,
       this.startdatetime,
-      this.finished});
+      this.finished,
+      this.appointmentTest});
 
   factory CareData.fromJson(Map<String, dynamic> json) =>
       _$CareDataFromJson(json);
 
   Map<String, dynamic> toJson() => _$CareDataToJson(this);
+}
+
+@JsonSerializable()
+class AppointmentSubDetails {
+  final int id;
+  final int apptid;
+  final String filename;
+  final String image;
+  final String linktype;
+  final int isdeleted;
+
+  AppointmentSubDetails(
+      {required this.id,
+      required this.apptid,
+      required this.filename,
+      required this.image,
+      required this.linktype,
+      required this.isdeleted});
+  factory AppointmentSubDetails.fromJson(Map<String, dynamic> json) =>
+      _$AppointmentSubDetailsFromJson(json);
+  Map<String, dynamic> toJson() => _$AppointmentSubDetailsToJson(this);
+}
+
+@JsonSerializable()
+class AppointmentNotes {
+  final int id;
+  final int apptid;
+  final String linktype;
+  final int isdeleted;
+
+  AppointmentNotes(
+      {required this.id,
+      required this.apptid,
+      required this.linktype,
+      required this.isdeleted});
+  factory AppointmentNotes.fromJson(Map<String, dynamic> json) =>
+      _$AppointmentNotesFromJson(json);
+  Map<String, dynamic> toJson() => _$AppointmentNotesToJson(this);
 }
--- packages/api/lib/src/caretaker/src/models/appointment/care_appointment_data.g.dart
+++ packages/api/lib/src/caretaker/src/models/appointment/care_appointment_data.g.dart
@@ -21,7 +21,37 @@ CareData _$CareDataFromJson(Map<String, dynamic> json) => CareData(
       startappointment: json['startappointment'] as String?,
       startdatetime: json['startdatetime'] as String?,
       finished: json['finished'] as String?,
-    );
+      appointmentTest: (json['appointment_test'] as List<dynamic>?)
+          ?.map(
+              (e) => AppointmentSubDetails.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    )
+      ..appointmentDiet = (json['appointment_diet'] as List<dynamic>?)
+          ?.map(
+              (e) => AppointmentSubDetails.fromJson(e as Map<String, dynamic>))
+          .toList()
+      ..appointmentDoctorsNote = (json['appointment_doctors_note']
+              as List<dynamic>?)
+          ?.map(
+              (e) => AppointmentSubDetails.fromJson(e as Map<String, dynamic>))
+          .toList()
+      ..appointmentPrescription = (json['appointment_prescription']
+              as List<dynamic>?)
+          ?.map(
+              (e) => AppointmentSubDetails.fromJson(e as Map<String, dynamic>))
+          .toList()
+      ..appointmentTestNotes =
+          (json['appointment_test_notes'] as List<dynamic>?)
+              ?.map((e) => AppointmentNotes.fromJson(e as Map<String, dynamic>))
+              .toList()
+      ..appointmentDietNotes =
+          (json['appointment_diet_notes'] as List<dynamic>?)
+              ?.map((e) => AppointmentNotes.fromJson(e as Map<String, dynamic>))
+              .toList()
+      ..appointmentDoctorsNoteNotes =
+          (json['appointment_doctors_note_notes'] as List<dynamic>?)
+              ?.map((e) => AppointmentNotes.fromJson(e as Map<String, dynamic>))
+              .toList();
 
 Map<String, dynamic> _$CareDataToJson(CareData instance) => <String, dynamic>{
       'apptid': instance.apptid,
@@ -38,4 +68,49 @@ Map<String, dynamic> _$CareDataToJson(CareData instance) => <String, dynamic>{
       'startappointment': instance.startappointment,
       'startdatetime': instance.startdatetime,
       'finished': instance.finished,
+      'appointment_test': instance.appointmentTest,
+      'appointment_diet': instance.appointmentDiet,
+      'appointment_doctors_note': instance.appointmentDoctorsNote,
+      'appointment_prescription': instance.appointmentPrescription,
+      'appointment_test_notes': instance.appointmentTestNotes,
+      'appointment_diet_notes': instance.appointmentDietNotes,
+      'appointment_doctors_note_notes': instance.appointmentDoctorsNoteNotes,
+    };
+
+AppointmentSubDetails _$AppointmentSubDetailsFromJson(
+        Map<String, dynamic> json) =>
+    AppointmentSubDetails(
+      id: json['id'] as int,
+      apptid: json['apptid'] as int,
+      filename: json['filename'] as String,
+      image: json['image'] as String,
+      linktype: json['linktype'] as String,
+      isdeleted: json['isdeleted'] as int,
+    );
+
+Map<String, dynamic> _$AppointmentSubDetailsToJson(
+        AppointmentSubDetails instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'apptid': instance.apptid,
+      'filename': instance.filename,
+      'image': instance.image,
+      'linktype': instance.linktype,
+      'isdeleted': instance.isdeleted,
+    };
+
+AppointmentNotes _$AppointmentNotesFromJson(Map<String, dynamic> json) =>
+    AppointmentNotes(
+      id: json['id'] as int,
+      apptid: json['apptid'] as int,
+      linktype: json['linktype'] as String,
+      isdeleted: json['isdeleted'] as int,
+    );
+
+Map<String, dynamic> _$AppointmentNotesToJson(AppointmentNotes instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'apptid': instance.apptid,
+      'linktype': instance.linktype,
+      'isdeleted': instance.isdeleted,
     };
